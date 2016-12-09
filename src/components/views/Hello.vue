c2ccd8c26bc52081e7145c15ecab5ed4893f4a28
Zhang Genmao <zgm4454@gmail.com>
2016-12-09 17:49:19 +0800

    添加生命周期

--- src/components/views/Hello.vue
+++ src/components/views/Hello.vue
@@ -10,10 +10,50 @@
 export default {
   name: 'hello',
   data () {
+    console.log('data')
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  /**
+   * [beforeCreate description]
+   * 加载 data 数据之前
+   */
+  beforeCreate () {
+    console.log('beforeCreate')
+  },
+  created () {
+    console.log('created')
+  },
+  beforeMount () {
+    console.log('beforeMount')
+  },
+  mounted () {
+    console.log('mounted')
+  },
+  beforeUpdate () {
+    console.log('beforeUpdate')
+  },
+  updated () {
+    console.log('updated')
+  },
+  /**
+   * [updated description]
+   * 状态激活时
+   */
+  activated () {
+    console.log('activated')
+  },
+  deactivated () {
+    console.log('deactivated')
+  },
+  beforeDestroy () {
+    console.log('beforeDestroy')
+  },
+  destroyed () {
+    console.log('destroyed')
   }
+
 }
 </script>
 
